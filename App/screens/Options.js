3d610a303a0b680b6bffe6d7b9492570515877f9
Michael Panter <makinglife1231@gmail.com>
2023-06-14 22:58:14 -0400

    added onpress to external links

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,40 +1,51 @@
 import React from "react";
 import Entypo from '@expo/vector-icons/Entypo';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, Linking, Alert } from 'react-native';
 
 import colors from "../constants/colors";
 import { RowItem, RowSeparator } from "../components/RowItem";
 
+const openUrl = (url) => {
+  return Linking.openURL(url).catch(() => {
+    Alert.alert('Something went wrong.', 'Please try again later.');
+  });
+}
+
 export default () => {
   return (
-    <SafeAreaView>
-      <RowItem 
-        title="Themes"
-        onPress={() => alert("todo!")}
-        rightIcon={
-          <Entypo name="chevron-right" size={20} color={colors.blue} />
-        }
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem 
+          title="Themes"
+          onPress={() => alert("todo!")}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
 
-      <RowSeparator />
-      
-      <RowItem 
-        title="React Native Basics"
-        onPress={() => alert("todo!")}
-        rightIcon={
-          <Entypo name="export" size={20} color={colors.blue} />
-        }
-      />
+        <RowSeparator />
+        
+        <RowItem 
+          title="React Native Basics"
+          onPress={
+            () => openUrl('https://reactnativeschool.com/react-native-basics')
+          }
+          rightIcon={
+            <Entypo name="export" size={20} color={colors.blue} />
+          }
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem 
-        title="React Native By Example"
-        onPress={() => alert("todo!")}
-        rightIcon={
-          <Entypo name="export" size={20} color={colors.blue} />
-        }
-      />
+        <RowItem 
+          title="React Native By Example"
+          onPress={() => openUrl('https://reactnativebyexample.com')}
+          rightIcon={
+            <Entypo name="export" size={20} color={colors.blue} />
+          }
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
